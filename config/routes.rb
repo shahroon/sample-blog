fc3ef7422c7757d79bf4717f5997c7389eb9c8e4
Shahroon Ali <shahroon.ali@gmail.com>
2021-03-02 16:25:56 +0500

    devise integration + routes setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :posts
+    root to: "dashboard#index"
+  end
+
+  root to: "blog#index"
 end
