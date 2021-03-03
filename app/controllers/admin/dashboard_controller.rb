8a50758f5128810bfadcfc2e3349830e0cfe36da
Shahroon Ali <shahroon.ali@gmail.com>
2021-03-03 17:23:24 +0500

    admin functionality + checks for roles

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,17 @@
 class Admin::DashboardController < ApplicationController
   layout "admin"
   before_action :authenticate_user!
+  before_action :check_admin, only: %i[ index ]
 
   def index
+    @users = User.first(10)
+  end
 
+  private
+
+  def check_admin
+    if !current_user.admin?
+      redirect_back(fallback_location: "/", notice: "You are not Admin, logout and login again as an Admin.")
+    end
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
-
+  scope :published, -> { where(published: true) }
 
   def is_owner(usr)
     self.user == usr
   end
+
 end
